4bb44763e0777d3de1fc708180eae3add1331dbc
Ana Celani Cañedo <anacelcan@gmail.com>
2021-11-24 20:18:36 -0300

    Saco comentarios en el api-route

--- api-route.php
+++ api-route.php
@@ -7,11 +7,9 @@ require_once 'Controller/ApiProductosController.php';
 $router = new Router();
 
 //comentarios
-//$router->addRoute('reviews', 'GET', 'ApiComentariosController', 'getComentarios');--->End Point al implementado, necesitamos el producto y tenemos 2 endpoinst iguales
 $router->addRoute('reviews/product/:ID', 'GET', 'ApiComentariosController', 'getComentarios'); //--> Traer todos los comentarios de un cierto proucto
 $router->addRoute('reviews/product/:ID', 'POST', 'ApiComentariosController', 'insertarComentario');
 $router->addRoute('reviews/:ID', 'GET', 'ApiComentariosController', 'getComentario');
-//$router->addRoute('reviews', 'POST', 'ApiComentariosController', 'insertarComentario'); //alta de comentario
 $router->addRoute('reviews/:ID', 'DELETE', 'ApiComentariosController', 'eliminarComentario'); //baja de comentario
 
 $router->addRoute('productos','GET','ApiProductosController','getProductos');
